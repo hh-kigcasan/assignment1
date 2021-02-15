317c1bb9d579e760688cb3760e724b723988c5fb
hh-kigcasan <hh.kigcasan@gmail.com>
2021-02-15 17:35:53 +0800

    Intentional bug

--- models/math-library.js
+++ models/math-library.js
@@ -8,7 +8,7 @@ class MathLibrary {
         return num1 + num2;
     };
     subtract(num1, num2) {
-        return num1 - num2;
+        return num1 * num2;
     }
     multiply(num1, num2) {
         return num1 * num2;
